fe888c9693abafa886ec726f55c05707b0adddf7
tinhour <fangfeng335@qq.com>
2016-08-14 19:48:11 +0800

    通过搜索关键字取得小说最新章节内容

--- superagent.js
+++ superagent.js
@@ -5,14 +5,15 @@ var request = require("superagent");
 var charsetCompoent = require('superagent-charset');
 var superagent = charsetCompoent(request);
 var iconv = require('iconv-lite');
-
-var browserAgent = {
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2810.2 Safari/537.36',
-}
+var async = require('async');
+var Log = require('./log.js').Log;
+var config = require('./config');
+var browserAgent = config.browserAgent;
+var baseDomain= config.baseDomain;
 var app = express();
 
 app.use(function(req, res, next) {
-  console.log(req.method, req.url);
+  Log(req.method, req.url);
   next();
 })
 
@@ -30,7 +31,7 @@ app.get('/latestUpdate', function(req, res, next) {
     if (err) {
       res.send(err)
     } else {
-      var r = analyseResult(sres.text)
+      var r = analyseListPage(sres.text)
       res.send(r);
     }
   })
@@ -43,36 +44,87 @@ app.get('/searchBook', function(req, res, next) {
     if (err) {
       res.send(err)
     } else {
-      var r = analyseResult(sres.text)
-      res.send(r);
+      var r = analyseListPage(sres.text);
+      var thisList;
+      if (r.data) {
+        for (var i in r.data) {
+          if (r.data[i].name == key && r.data[i].link) {
+            thisList = r.data[i];
+            break;
+          }
+        }
+      }
+      Log("searchResult", thisList)
+      if (!thisList) {
+        res.send({
+          error: "未找到书本:《" + key + "》"
+        })
+      } else {
+        //todo get thisList.link to get the result list
+        var pageListLink = baseDomain + thisList.link;
+        getPageContent(pageListLink, function(err, sres) {
+            if (err) {
+              res.send(err)
+            } else {
+              var r = analyseItemPage(sres.text);
+              //res.send(r);
+              //console.log("get analyseItemPage",r)
+              //todo goto artilce page ViewBook.aspx
+              if (r && r.data && r.data.length > 2) {
+                //todo analyse all links now just get first as newspost
+                //TODO: use asyn to get corect page.
+                var pageDetailLink = baseDomain + r.data[1].link;
+                getPageContent(pageDetailLink, function(err, sres) {
+                  if (err) {
+                    res.send(err)
+                  } else {
+                    //todo anlaylise 
+                    //Log("detail", sres.text);
+                   var targetSrc= analyseContentPage(sres.text)
+                     Log("targetSrc",targetSrc)
+                     if(targetSrc.error){
+                        res.send(targetSrc)
+                     }else{
+                        getPageContent(targetSrc.data,function (err,sres) {
+                          if(err){
+                            res.send(err)
+                          }else{
+                            // Log("true html:",sres.text)
+                            //res.send(sres.text)
+                            //todo anlayse true text
+                            //content
+                            var r=analyseArticleContent(sres.text)
+                            r.artilceName=key;
+                            res.send(r);
+                          }
+                        })
+                     }
+                  }
+                })
+              } else {
+                res.send({
+                  error: "没有章节:" + key + ""
+                })
+              }
+            }
+          })
+          //res.send(thisList);
+      }
     }
   })
 });
-
-function Log(argument) {
-  console.log.apply(this, arguments)
-}
-
 function getPageContent(url, callback) {
   var startTime = new Date();
-  superagent.head(url).set(browserAgent).end(function(err, sres) {
-    var endGetHeadTime = new Date();
-    Log("get [%s] head cost time %ds", url, (endGetHeadTime - startTime) / 1000)
-    if (err) {
-      return callback(err);
-    }
-    var contentType = sres.header['content-type'];
-    var characters = contentType.match(/charset=([\w]+)$/)[1] || "utf-8";
-    superagent.get(url).set(browserAgent).charset(characters)
+  superagent.get(url).set(browserAgent).charset()
       .end(function(err, sres) {
         if (err) {
+          Log("getPageContent [%s] error: %s",url,err)
           return callback(err);
         }
         var endGetPageTime = new Date();
-        Log("get[%s] page cost time %ds", url, (endGetPageTime - endGetHeadTime) / 1000)
+        Log("get [%s] page cost time %ds", url, (endGetPageTime - startTime) / 1000)
         callback(null, sres)
       })
-  })
 }
 
 function searchBook(key, callback) {
@@ -96,24 +148,25 @@ function searchBook(key, callback) {
     })  
 }
 
-function analyseResult(html) {
+function analyseListPage(html) {
   var $ = cheerio.load(html);
   var containTable = null;
   var result = []
-  Log($("body").text())
-  $('table').each(function(idx, element) {
+  for(var idx=0;idx<$('table').length;idx++){
+    var element=$('table')[idx]
     var tableText = $(element).text();
-    if (/最新章节/.test(tableText) && /更新时间/.test(tableText)) {
+    if (/更新时间/.test(tableText)) {
       containTable = element;
-      return;
+      break;
     }
-  })
+  }
+  // Log("contain>table",$(containTable).text())
   if (containTable) {
     $(containTable).children("tr").each(function(i, element) {
       var tdEle = $(element).children("td");
       var item = {
         name: $(tdEle).eq(0).text().trim(),
-        link: $(tdEle).eq(0).children("a").attr("href"),
+        link: $(tdEle).eq(1).children("a").attr("href"),
         newsPost: $(tdEle).eq(1).text().trim(),
         updateTie: $(tdEle).eq(2).text().trim()
       }
@@ -129,8 +182,71 @@ function analyseResult(html) {
   }
 }
 
+function analyseItemPage(html) {
+  var $ = cheerio.load(html);
+  var containTable = null;
+  var result = []
+  for(var idx=0;idx<$('table').length;idx++){
+    var element=$('table')[idx]
+    var tableText = $(element).text();
+    if (/更新时间/.test(tableText)) {
+      containTable = element;
+      break;
+    }
+  }
+  // Log($("containTable").text())
+  if (containTable) {
+    $(containTable).children("tr").each(function(i, element) {
+      var tdEle = $(element).children("td");
+      var item = {
+        link: $(tdEle).eq(0).children("a").attr("href"),
+        name: $(tdEle).eq(0).text().trim(),
+        updateTie: $(tdEle).eq(3).text().trim()
+      }
+      result.push(item);
+    })
+    return {
+      data: result
+    }
+  } else {
+    return {
+      error: "no result"
+    }
+  }
+}
+
+function analyseContentPage(html) {
+  var $ = cheerio.load(html);
+  var targetSrc=$("frame[src^='http']").attr("src");
+  if(!targetSrc){
+    return {
+        error: "no frame contains main"
+      }
+  }else{
+    return {
+        data: targetSrc
+      }
+  }
+}
+
+function analyseArticleContent(html) {
+  var $ = cheerio.load(html);
+  var content = $("[id*='content']");
+  Log("content length:", content.length)
+  if (!content.length) {
+    return {
+      error: "not found conent"
+    }
+  } else {
+    return {
+      data: content.text().trim()
+    }
+  }
+}
+
+
 var server = app.listen(3003, function() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Scraw App listening at http://%s:%s', host, port);
+  Log('Scraw App listening at http://%s:%s', host, port);
 });
